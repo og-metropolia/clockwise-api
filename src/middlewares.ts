3c04aa5cf92260dca65939d90692f458d34bf6c5
4MBL <74970679+4MBL@users.noreply.github.com>
2024-02-16 10:15:31 +0200

    fix `dev` script crashes

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -2,17 +2,15 @@ import { NextFunction, Request, Response } from 'express';
 import CustomError from './classes/CustomError';
 import { ErrorResponse } from './types/MessageTypes';
 
-const notFound = (req: Request, res: Response, next: NextFunction) => {
+const notFound = (req: Request, _res: Response, next: NextFunction) => {
   const error = new CustomError(`Not Found - ${req.originalUrl}`, 404);
   next(error);
 };
 
 const errorHandler = (
   err: CustomError,
-  req: Request,
+  _req: Request,
   res: Response<ErrorResponse>,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  next: NextFunction,
 ) => {
   const statusCode = err.status !== 200 ? err.status || 500 : 500;
   res.status(statusCode).json({
